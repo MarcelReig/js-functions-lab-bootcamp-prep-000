81b62825106ba4f38bdc8ee48b6994909a3328d7
Marcel Reig <marcelreig@gmail.com>
2021-06-13 12:19:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 // write your code below!
-var name = "you";
-var holidays = "Independence Day"
-
 function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(name) {
+function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
 
-function happyCustomHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}`
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(days, holiday) {
-  return `It\'s ${days} days untill ${holiday}!`
+
+function holidayCountdown(holiday, days){
+  return `It\'s ${days} days until ${holiday}!`
 }
